9f5b336c43c2d00f2a3c44ea5c341692c41ecb6a
Ángel Barrera Sánchez <angel@sighup.io>
2021-02-25 14:06:55 +0100

    Add v1.5.0 features to AKS SIGHUP Cloud installer

--- modules/aks/data.tf
+++ modules/aks/data.tf
@@ -4,9 +4,3 @@ data "azurerm_subscription" "current" {
 data "azurerm_resource_group" "aks" {
   name = var.resource_group_name
 }
-
-data "azurerm_subnet" "subnetwork" {
-  name                 = var.subnetworks[0]
-  virtual_network_name = var.network
-  resource_group_name  = var.resource_group_name
-}
