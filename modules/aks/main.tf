c65002f097151275f12cdc81a339fcbed33ffc8e
Ángel Barrera Sánchez <angel@sighup.io>
2021-05-26 09:52:15 +0200

    Pin terraform version 0.15.4

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = "0.15.4"
   required_providers {
-    kubernetes = "= 1.13.3"
-    azuread    = "= 1.4.0"
-    azurerm    = "= 2.48.0"
-    random     = "= 3.1.0"
-    null       = "= 3.1.0"
+    kubernetes = "1.13.3"
+    azuread    = "1.5.0"
+    azurerm    = "2.60.0"
+    random     = "3.1.0"
+    null       = "3.1.0"
   }
 }
