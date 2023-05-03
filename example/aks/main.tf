156c2111a1d7e7162def7840bdae37241d6ab49d
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-05-03 16:42:08 +0200

    fix(example,aks): add kubernetes provider definition

--- example/aks/main.tf
+++ example/aks/main.tf
@@ -3,6 +3,19 @@ provider "azurerm" {
   }
 }
 
+provider "kubernetes" {
+  host = data.azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate,
+  )
+  client_key = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key,
+  )
+  cluster_ca_certificate = base64decode(
+    data.azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate,
+  )
+}
+
 module "my-cluster" {
   source = "../../modules/aks"
 
