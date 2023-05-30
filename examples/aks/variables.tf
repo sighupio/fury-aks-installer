8362ecbeede655bffa8cc28193e869e93564c6e9
Simone Bruzzese <sbruzzese@sighup.io>
2023-05-30 10:31:11 +0200

    Remove default variables from examples

--- examples/aks/variables.tf
+++ examples/aks/variables.tf
@@ -1,19 +1,15 @@
 variable "network" {
   type    = string
-  default = "aks-installer"
 }
 
 variable "subnetworks" {
   type    = list(string)
-  default = ["aks-installer-aks"]
 }
 
 variable "resource_group_name" {
   type    = string
-  default = "aks-installer-aks-rg"
 }
 
 variable "location" {
   type    = string
-  default = "westeurope"
 }
--- examples/networking/variables.tf
+++ examples/networking/variables.tf
@@ -1,21 +1,17 @@
 variable "name" {
   type        = string
   description = "Common project name"
-  default = "fury"
 }
 
 variable "vnet_cidr" {
   type    = string
-  default = "10.100.0.0/16"
 }
 
 variable "ssh_public_key_path" {
   type        = string
   description = "ssh public key to connect to bastion and nodes"
-  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "tags" {
   type    = map(any)
-  default = {}
 }
