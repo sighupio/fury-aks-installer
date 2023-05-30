6abd659b80c92aed3f55e7141df805bb3ce8895e
Simone Bruzzese <sbruzzese@sighup.io>
2023-05-30 10:32:44 +0200

    Remove strict versions lock

--- modules/vnet-and-vpn/main.tf
+++ modules/vnet-and-vpn/main.tf
@@ -16,27 +16,27 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.44.1"
+      version = "~> 3.44.1"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.0.0"
+      version = "~> 2.0.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "3.2.1"
+      version = "~> 3.2.1"
     }
     external = {
       source  = "hashicorp/external"
-      version = "2.0.0"
+      version = "~> 2.0.0"
     }
     template = {
       source  = "hashicorp/template"
-      version = "2.2.0"
+      version = "~> 2.2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 3.4.3"
     }
   }
 }
